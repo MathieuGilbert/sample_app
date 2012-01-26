dcd887bcb224f83ea5f0c2e475d7587b11f082a5
Mathieu Gilbert <matnarak@gmail.com>
2012-01-25 22:09:38 -0700

    done adding layouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 SampleApp::Application.routes.draw do
+  match '/contact' => 'pages#contact'
+  match '/about'   => 'pages#about'
+  match '/help'    => 'pages#help'
+    
   get "pages/home"
   get "pages/contact"
   get "pages/about"
   get "pages/help"
-
+  
   root :to => "pages#home"
+  
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
