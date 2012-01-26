feb2c2ef8218661270874685b2614def09df71fd
Mathieu Gilbert <matnarak@gmail.com>
2012-01-25 19:11:35 -0700

    fixed pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
   # return title on a per-page basis
-  def title
+  def full_title(page_title)
     base_title = "Ruby on Rails Tutorial Sample App"
-    if @title.nil?
+    if page_title.empty?
       base_title
     else
-      "#{base_title} | #{@title}"
+      "#{base_title} | #{page_title}"
     end
   end
 end
